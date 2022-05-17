be6aebfff1b461d884709965c9bcb560a88b1113
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-17 19:55:36 +0300

    feat: add sync async func setTimeout

--- app.js
+++ app.js
@@ -1,12 +1,68 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
+const http = require("http");
+ 
+let message = "Hello World!";
+http.createServer(function(request,response){
+     
+    console.log(message);
+    response.end(message);
+     
+}).listen(3000, "127.0.0.1",()=>{
+    console.log("Сервер начал прослушивание запросов");
+});
 
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
 
-// node app.js Tom 23
+// =====================sync
+
+
+// function displaySync(data){
+//     console.log(data);
+// }
+ 
+// console.log("Начало работы программы");
+ 
+// displaySync("Обработка данных...");
+ 
+// console.log("Завершение работы программы");
+
+// =====================async
+// function display(data, callback){
+ 
+    // с помощью случайного числа определяем ошибку
+//     var randInt = Math.random() * (10 - 1) + 1;
+//     var err = randInt>5? new Error("Ошибка выполнения. randInt больше 5"): null;
+     
+//     setTimeout(function(){
+//         callback(err, data);
+//     }, 0);
+// }
+ 
+// console.log("Начало работы программы");
+ 
+// display("Обработка данных...", function (err, data){
+ 
+//     if(err) throw err;
+//     console.log(data);
+// });
+ 
+// console.log("Завершение работы программы");
+
+// =====================async
+
+function displaySync(callback){
+    callback();
+}
+ 
+console.log("Начало работы программы");
+ 
+setTimeout(function(){
+         
+        console.log("timeout 500");
+}, 500);
+ 
+setTimeout(function(){
+         
+        console.log("timeout 100");
+}, 100);
+ 
+displaySync(function(){console.log("without timeout")});
+console.log("Завершение работы программы");
